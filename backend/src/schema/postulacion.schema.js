b6993c6adb2990f376a70ac95765312992161b3b
Lucas Hernandez <lucas.hernandez2001@alumnos.ubiobio.cl>
2023-11-22 22:04:01 -0300

    Validacion nombre postulante

--- backend/src/schema/postulacion.schema.js
+++ backend/src/schema/postulacion.schema.js
@@ -16,7 +16,7 @@ const postulacionBodySchema = Joi.object({
             "string.base": "El nombre del postulante debe ser de tipo string.",
             "string.empty": "El nombre del postulante no puede estar vacío.",
             "any.required": "El nombre del postulante es obligatorio.",
-            "string.pattern.base": "El nombre del postulante no puede contener números"
+            "string.pattern.base": "El nombre del postulante no puede contener números ni simbolos"
         }),
     rutPostulante: Joi.string()
         .required()
